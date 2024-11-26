fe06de182bbfc490ec4c38a3690f858b89a44e6c
nethaka-sades <sades.dev01@gmail.com>
2024-11-27 00:37:17 +0530

    Link component used

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -103,7 +103,7 @@ export function Navbar() {
                     <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                       <li className="row-span-3">
                         <NavigationMenuLink asChild>
-                          <a
+                          <Link
                             className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                             href="/"
                           >
@@ -113,7 +113,7 @@ export function Navbar() {
                             <p className="text-sm leading-tight text-muted-foreground">
                               About Sri Bodhiraja Dhamma School
                             </p>
-                          </a>
+                          </Link>
                         </NavigationMenuLink>
                       </li>
                       <ListItem href="/docs" title="Bellanvila Temple">
