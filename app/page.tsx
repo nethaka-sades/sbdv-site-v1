20c939b3950ad1e52636806b09928ce488293104
nethaka-sades <sades.dev01@gmail.com>
2024-11-29 02:36:04 +0530

    loading support added + hero section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,45 @@
 /*
- *  
+ *
  *   Copyright 2024 Nethaka De Saram
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the 'License');
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an 'AS IS' BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  
- *  
- *  
- *  
+ *
+ *
+ *
+ *
  *  Created on Sat Nov 23 2024
- *  
+ *
  */
+
+import Hero from "@/components/hero";
+import ImageCarousel from "@/components/ImageCarousel";
+import LoadingScreen from "@/components/LoadingScreen";
 
 export default function Home() {
   return (
     <div>
-      <div className="text-3xl text-center">Home</div>
+      <LoadingScreen />
+      <main>
+        <Hero />
+        <br />
+        <Hero />
+        <br />
+        <Hero />
+        <br />
+        <Hero />
+        <br />
+        <Hero />
+      </main>
     </div>
   );
 }
