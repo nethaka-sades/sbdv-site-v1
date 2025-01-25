a942aa6d71db64bd39aebb585ae525af90ed0136
nethaka-sades <sades.dev01@gmail.com>
2025-01-26 02:09:56 +0530

    User profile updating page... not yet finished

--- app/lms/dashboard/update_profile/page.tsx
+++ app/lms/dashboard/update_profile/page.tsx
@@ -31,7 +31,7 @@ import { Label } from "@radix-ui/react-label";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@radix-ui/react-separator";
 import { Checkbox } from "@/components/ui/checkbox";
-import { useAuthContext } from "@/app/context/AuthContext";
+import { useAuthContext } from "@/app/_context/AuthContext";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Card,
@@ -45,6 +45,7 @@ import {
 export default function signup() {
   const { user } = useAuthContext();
   const { register } = useAuthContext();
+  const { updateData } = useAuthContext();
   const [displayName, setDisplayName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -53,9 +54,6 @@ export default function signup() {
   const [adminNo, setAdminNo] = useState("");
   const [address, setAddress] = useState("");
   const [phoneNo, setPhoneNo] = useState("");
-  const [termsAccepted, setTermsAccepted] = useState<"indeterminate" | boolean>(
-    false
-  );
   const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
 
@@ -70,20 +68,16 @@ export default function signup() {
   const updateProfile = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
 
-    const regResult = await register(
-      email,
-      password,
-      displayName,
-      adminYear,
-      adminNo,
-      address,
-      phoneNo
-    );
-    if (regResult.success) {
-      setErrorMessage("");
-      router.push("/lms/dashboard");
+    if(user){
+      const updateResult = await updateData(user.uid, displayName, address, phoneNo);
+      if(updateResult.success){
+        console.log("user data updated")
+        router.push("/lms/dashboard");
+      } else {
+        console.error(updateResult.error);
+      }
     } else {
-      setErrorMessage("Registering Failed : " + regResult.error);
+      cancelUpdate(event);
     }
   };
 
@@ -92,13 +86,6 @@ export default function signup() {
 
     if (password !== confirmPassword) {
       setErrorMessage("Passwords do not match.");
-      return;
-    } else {
-      setErrorMessage("");
-    }
-
-    if (!termsAccepted) {
-      setErrorMessage("You must agree to the terms and conditions.");
       return;
     } else {
       setErrorMessage("");
@@ -151,7 +138,6 @@ export default function signup() {
                         </Label>
                         <Input
                           onChange={(e) => setDisplayName(e.target.value)}
-                          required
                           type="text"
                           id="displayName"
                           placeholder={user?.displayName || "Your Name"}
@@ -163,7 +149,6 @@ export default function signup() {
                         </Label>
                         <Input
                           onChange={(e) => setAddress(e.target.value)}
-                          required
                           type="text"
                           id="address"
                           placeholder={user?.address || "Your Address"}
@@ -175,7 +160,6 @@ export default function signup() {
                         </Label>
                         <Input
                           onChange={(e) => setPhoneNo(e.target.value)}
-                          required
                           type="text"
                           id="phoneNo"
                           placeholder={user?.phoneNo || "Your Telephone Number"}
@@ -251,130 +235,6 @@ export default function signup() {
                 </Card>
               </TabsContent>
             </Tabs>
-
-            <form onSubmit={updateProfile}>
-              <div className="mb-4">
-                <Label htmlFor="displayName" className="sr-only">
-                  Your Name
-                </Label>
-                <Input
-                  onChange={(e) => setDisplayName(e.target.value)}
-                  required
-                  type="text"
-                  id="displayName"
-                  placeholder="Your Name"
-                />
-              </div>
-              <div className="mb-4">
-                <Label htmlFor="email" className="sr-only">
-                  Email
-                </Label>
-                <Input
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  type="email"
-                  id="email"
-                  placeholder="Email"
-                />
-              </div>
-              <div className="mb-4">
-                <Label htmlFor="password" className="sr-only">
-                  Password
-                </Label>
-                <Input
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  type="password"
-                  id="password"
-                  placeholder="Password"
-                />
-              </div>
-              <div className="mb-4">
-                <Label htmlFor="confirmPassword" className="sr-only">
-                  Confirm Password
-                </Label>
-                <Input
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  required
-                  type="password"
-                  id="confirmPassword"
-                  placeholder="Confirm Password"
-                />
-              </div>
-              <Separator asChild className="my-6">
-                <div className="py-3 flex items-center text-xs text-muted-foreground uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:before:border-gray-700 dark:after:border-gray-700">
-                  Basic Details
-                </div>
-              </Separator>
-              <div className="mb-4">
-                <Label htmlFor="adminYear" className="sr-only">
-                  Your Admission Year
-                </Label>
-                <Input
-                  onChange={(e) => setAdminYear(e.target.value)}
-                  required
-                  type="text"
-                  id="adminYear"
-                  placeholder="Your Admission Year"
-                />
-              </div>
-              <div className="mb-4">
-                <Label htmlFor="adminNo" className="sr-only">
-                  Your Admission Number
-                </Label>
-                <Input
-                  onChange={(e) => setAdminNo(e.target.value)}
-                  required
-                  type="text"
-                  id="adminNo"
-                  placeholder="Your Admission Number"
-                />
-              </div>
-              <div className="mb-4">
-                <Label htmlFor="address" className="sr-only">
-                  Your Address
-                </Label>
-                <Input
-                  onChange={(e) => setAddress(e.target.value)}
-                  required
-                  type="text"
-                  id="address"
-                  placeholder="Your Address"
-                />
-              </div>
-              <div className="mb-4">
-                <Label htmlFor="phoneNo" className="sr-only">
-                  Your Telephone Number
-                </Label>
-                <Input
-                  onChange={(e) => setPhoneNo(e.target.value)}
-                  required
-                  type="text"
-                  id="phoneNo"
-                  placeholder="Your Telephone Number"
-                />
-              </div>
-              <div className="flex items-center mb-4">
-                <Checkbox
-                  id="terms"
-                  checked={termsAccepted}
-                  onCheckedChange={(checked) =>
-                    setTermsAccepted(checked === true)
-                  }
-                />
-                <Label htmlFor="terms" className="ml-2">
-                  I confirm that above information are accurate and true.
-                </Label>
-              </div>
-              {errorMessage && (
-                <p className="text-red-500 mb-4">{errorMessage}</p>
-              )}
-              <div className="grid">
-                <Button type="submit" disabled={!termsAccepted}>
-                  Register
-                </Button>
-              </div>
-            </form>
           </div>
         </div>
       </section>
