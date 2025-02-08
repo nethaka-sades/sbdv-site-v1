b54ad6e8d062116dc365fa3d57be6d24ccdfe8c4
nethaka-sades <sades.dev01@gmail.com>
2025-02-08 12:40:14 +0530

    update

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Settings(){
     return(
-        <main className="py-32 min-h-screen lg:max-w-[300px] mx-auto">
+        <main className="py-32 min-h-screen max-w-[300px] mx-auto">
             <div className="my-5 text-6xl font-bold text-center">Settings</div>
             <div className="p-5 flex flex-col items-center gap-y-5 border rounded-md">
                 <Link href={"/dashboard/update-profile"}><Button variant={"outline"} className="text-primary-foreground"><User />Update Profile</Button></Link>
