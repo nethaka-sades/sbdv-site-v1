2996e8de5002544900793783715bb9209bf26246
nethaka-sades <sades.dev01@gmail.com>
2025-03-22 21:57:30 +0530

    small error fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -74,7 +74,7 @@ export default async function DashboardPage() {
 
   if (notices_error) {
     console.log(notices_error);
-    throw error;
+    throw notices_error;
   }
 
   const { data: marks_data, error: marks_data_error } = await supabase
