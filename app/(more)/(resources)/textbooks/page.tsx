3b1be68d5a5288835acbcac1b1d3ad18e901851f
nethaka-sades <sades.dev01@gmail.com>
2025-04-03 00:44:05 +0530

    update

--- app/(more)/(resources)/textbooks/page.tsx
+++ app/(more)/(resources)/textbooks/page.tsx
@@ -27,33 +27,37 @@ import Link from "next/link";
 export default async function TextbookDownloadPage() {
   var textbooks = null;
   try {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const { data: textbooks_g, error: textbook_error } = await supabase
-      .from("textbooks")
-      .select("*")
-      .order("order", { ascending: true });
+  const { data: textbooks_g, error: textbook_error } = await supabase
+    .from("textbooks")
+    .select("*")
+    .order("order", { ascending: true });
 
-    if (textbook_error) {
-      <ErrorComp />;
-    } else {
-      textbooks = textbooks_g;
-    }
-  } catch {
-    return <ErrorComp />;
+  if (textbook_error) {
+    return <ErrorComp />
+  } else {
+    textbooks = textbooks_g;
   }
+} catch {
+  return <ErrorComp />
+}
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-12 px-10">
       {textbooks && textbooks.length > 0 ? (
         textbooks.map((textbook) => (
-          <Link href={textbook.link} key={textbook.id}>
-            <div className="p-3 max-w-sm rounded overflow-hidden shadow-lg border border-neutral-600 cursor-pointer hover:bg-orange-600">
-              <div className="p-4">
-                <h3 className="text-xl font-semibold">{textbook.name}</h3>
-              </div>
-            </div>
-          </Link>
+          <Link
+          href={textbook.link}
+          key={textbook.id}
+        >
+        <div
+          className="p-3 max-w-sm rounded overflow-hidden shadow-lg border border-neutral-600 cursor-pointer hover:bg-orange-600"        >
+          <div className="p-4">
+            <h3 className="text-xl font-semibold">{textbook.name}</h3>
+          </div>
+        </div>
+        </Link>
         ))
       ) : (
         <p>No textbooks found.</p>
