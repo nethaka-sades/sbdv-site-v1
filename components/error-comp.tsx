c6ab46924ee1fca455af06d9d4125149be7a6485
nethaka-sades <sades.dev01@gmail.com>
2025-04-03 00:58:18 +0530

    polished

--- components/error-comp.tsx
+++ components/error-comp.tsx
@@ -25,7 +25,7 @@ export default function ErrorComp(){
     return(
         <div className="min-h-screen flex items-center justify-center">
         <div className="text-center space-y-6 p-6">
-          <h1 className="text-4xl font-bold text-white">Error Occured</h1>
+          <h1 className="text-4xl font-bold text-red-600">Error Occured</h1>
           <p className="text-lg text-neutral-200 max-w-md">
             Please refresh the page or contact support
           </p>
